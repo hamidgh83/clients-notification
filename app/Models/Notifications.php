0a1457ab4934d43a989589cc677bbf892356f6fa
Hamid Ghorashi <h.ghorashi@gmail.com>
2022-03-23 11:45:50 +0400

    Modify notifications table to add status column.

--- app/Models/Notifications.php
+++ app/Models/Notifications.php
@@ -7,8 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notifications extends Model
 {
-    const TYPE_SMS   = 'sms';
-    const TYPE_EMAIL = 'email';
+    const TYPE_SMS       = 'sms';
+    const TYPE_EMAIL     = 'email';
+    const STATUS_PENDING = 'pending';
+    const STATUS_SENT    = 'sent';
+
+    protected $fillable = [
+        "user_id",
+        "channel",
+        "content",
+        "status",
+    ];
 
     use HasFactory;
 }
---
+++ database/migrations/2022_03_22_223346_modify_notifications_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Notifications;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('notifications', function (Blueprint $table) {
+            $table->string('status')->default(Notifications::STATUS_PENDING);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+    }
+};
