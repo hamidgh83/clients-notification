52db8dd3abe3bdca778e72c8da668e9fafcd83e1
Hamid Ghorashi <h.ghorashi@gmail.com>
2022-03-27 14:45:32 +0400

    Increate phone_number column length and update validation..

--- app/Http/Requests/CreateClientRequest.php
+++ app/Http/Requests/CreateClientRequest.php
@@ -27,7 +27,7 @@ class CreateClientRequest extends FormRequest
             'first_name'   => ['required', 'string', 'max:20'],
             'last_name'    => ['required', 'string', 'max:20'],
             'email'        => ['required', 'email', 'unique:users'],
-            'phone_number' => ['required', 'numeric', 'digits_between:6,10'],
+            'phone_number' => ['required', 'regex:/^\+[1-9]\d{1,14}$/'],
         ];
     }
 }
